71f94d91cbafb09e133ce158673daa57efbb2e97
rintar <rin.szlo28@gmail.com>
2014-02-10 13:24:47 +0900

    金曜ならメッセージを変えてみる

--- today.py
+++ today.py
@@ -2,7 +2,9 @@
 import datetime
 
 today = datetime.datetime.now()
-if today.weekday() < 5:
+if today.weekday() < 4:
     print('仕事')
+elif today.weekday() == 4:
+    print('明日は休みだ')
 else:
     print('休み')
